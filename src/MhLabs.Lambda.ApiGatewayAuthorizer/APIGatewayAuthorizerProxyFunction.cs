f7229e6c18967c836b96ee1a622c4dc28a9be569
Lars <lars@mathem.se>
2017-08-13 20:40:19 +0200

    Fake local ping request to keep lambda warm

--- src/MhLabs.Lambda.ApiGatewayAuthorizer/APIGatewayAuthorizerProxyFunction.cs
+++ src/MhLabs.Lambda.ApiGatewayAuthorizer/APIGatewayAuthorizerProxyFunction.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -6,6 +7,8 @@ using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.AspNetCoreServer;
 using Amazon.Lambda.AspNetCoreServer.Internal;
 using Amazon.Lambda.Core;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace MhLabs.Lambda.ApiGatewayAuthorizer
 {
@@ -14,9 +17,23 @@ namespace MhLabs.Lambda.ApiGatewayAuthorizer
         [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
         public async Task<APIGatewayProxyResponse> FunctionHandlerAsync(APIGatewayAuthorizerProxyRequest request, ILambdaContext lambdaContext)
         {
+            
+            Console.WriteLine(JsonConvert.SerializeObject(request));
+            if (string.IsNullOrEmpty(request.HttpMethod))
+            {
+                request.HttpMethod = "GET";
+                request.Path = "/ping";
+                request.Headers = new Dictionary<string, string> {{"Host", "localhost"}};
+                request.RequestContext= new AuthorizerRequestContext();
+
+                lambdaContext.Logger.LogLine("Keep-alive invokation");
+                lambdaContext = new DummyContext(lambdaContext);
+               
+            }
             lambdaContext.Logger.LogLine($"Incoming {request.HttpMethod} requests to {request.Path}");
             var features = new InvokeFeatures();
             MarshallRequest(features, request);
+
             var context = this.CreateContext(features);
 
             if (request.RequestContext.Authorizer != null)
@@ -33,6 +50,49 @@ namespace MhLabs.Lambda.ApiGatewayAuthorizer
             var response = await this.ProcessRequest(lambdaContext, context, features);
 
             return response;
+        }
+    }
+
+    public class DummyContext : ILambdaContext
+    {
+        public string AwsRequestId { get; }
+        public IClientContext ClientContext { get; }
+        public string FunctionName { get; }
+        public string FunctionVersion { get; }
+        public ICognitoIdentity Identity { get; }
+        public string InvokedFunctionArn { get; }
+        public ILambdaLogger Logger { get; set; }
+        public string LogGroupName { get; }
+        public string LogStreamName { get; }
+        public int MemoryLimitInMB { get; }
+        public TimeSpan RemainingTime { get; }
+
+        public DummyContext(ILambdaContext fromContext)
+        {
+            AwsRequestId = fromContext.AwsRequestId;
+            ClientContext = fromContext.ClientContext;
+            FunctionName = fromContext.FunctionName;
+            FunctionVersion = fromContext.FunctionVersion;
+            Identity = fromContext.Identity;
+            InvokedFunctionArn = fromContext.InvokedFunctionArn;
+            LogGroupName = fromContext.LogGroupName;
+            LogStreamName = fromContext.LogStreamName;
+            MemoryLimitInMB = fromContext.MemoryLimitInMB;
+            RemainingTime = fromContext.RemainingTime;
+            Logger = new NullLogger();
+        }
+    }
+
+    public class NullLogger : ILambdaLogger
+    {
+        public void Log(string message)
+        {
+            
+        }
+
+        public void LogLine(string message)
+        {
+            
         }
     }
 }
