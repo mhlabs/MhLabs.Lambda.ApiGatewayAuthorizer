ba51fb3761ccaf7bd3c6c7733a174bb2d030bdb8
Lars <lars@mathem.se>
2017-09-21 12:59:35 +0200

    adding gzip stuff

--- src/MhLabs.Lambda.ApiGatewayAuthorizer/APIGatewayAuthorizerProxyFunction.cs
+++ src/MhLabs.Lambda.ApiGatewayAuthorizer/APIGatewayAuthorizerProxyFunction.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Net.Http;
 using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.AspNetCoreServer;
 using Amazon.Lambda.AspNetCoreServer.Internal;
 using Amazon.Lambda.Core;
 using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
 using Newtonsoft.Json;
 
 namespace MhLabs.Lambda.ApiGatewayAuthorizer
@@ -17,18 +22,18 @@ namespace MhLabs.Lambda.ApiGatewayAuthorizer
         [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
         public async Task<APIGatewayProxyResponse> FunctionHandlerAsync(APIGatewayAuthorizerProxyRequest request, ILambdaContext lambdaContext)
         {
-            
+
             Console.WriteLine(JsonConvert.SerializeObject(request));
             if (string.IsNullOrEmpty(request.HttpMethod))
             {
                 request.HttpMethod = "GET";
                 request.Path = "/ping";
-                request.Headers = new Dictionary<string, string> {{"Host", "localhost"}};
-                request.RequestContext= new AuthorizerRequestContext();
+                request.Headers = new Dictionary<string, string> { { "Host", "localhost" } };
+                request.RequestContext = new AuthorizerRequestContext();
 
                 lambdaContext.Logger.LogLine("Keep-alive invokation");
                 lambdaContext = new DummyContext(lambdaContext);
-               
+
             }
             lambdaContext.Logger.LogLine($"Incoming {request.HttpMethod} requests to {request.Path}");
             var features = new InvokeFeatures();
@@ -46,13 +51,50 @@ namespace MhLabs.Lambda.ApiGatewayAuthorizer
             // Add along the Lambda objects to the HttpContext to give access to Lambda to them in the ASP.NET Core application
             context.HttpContext.Items[LAMBDA_CONTEXT] = lambdaContext;
             context.HttpContext.Items[APIGATEWAY_REQUEST] = request;
-            
+
             var response = await this.ProcessRequest(lambdaContext, context, features);
-            
-            response.IsBase64Encoded = Environment.GetEnvironmentVariable("IsBase64Encoded") != null && bool.Parse(Environment.GetEnvironmentVariable("IsBase64Encoded"));
+            var useGzip = Environment.GetEnvironmentVariable("UseGzip") != null && bool.Parse(Environment.GetEnvironmentVariable("UseGzip"));
+            if (useGzip && context.HttpContext.Request.Method != HttpMethod.Options.Method)
+            {
+                response.IsBase64Encoded = true;
+                var buffer = Zip(response.Body);
+                response.Body = Convert.ToBase64String(buffer);
+                response.Headers.Add(HeaderNames.ContentEncoding, "gzip");
+            }
+
             return response;
         }
+        public static void CopyTo(Stream src, Stream dest)
+        {
+            byte[] bytes = new byte[4096];
+
+            int cnt;
+
+            while ((cnt = src.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                dest.Write(bytes, 0, cnt);
+            }
+        }
+
+        public static byte[] Zip(string str)
+        {
+            var bytes = Encoding.UTF8.GetBytes(str);
+
+            using (var msi = new MemoryStream(bytes))
+            using (var mso = new MemoryStream())
+            {
+                using (var gs = new GZipStream(mso, CompressionMode.Compress))
+                {
+                    CopyTo(msi, gs);
+                }
+
+                return mso.ToArray();
+            }
+        }
+
     }
+
+
 
     public class DummyContext : ILambdaContext
     {
@@ -88,12 +130,12 @@ namespace MhLabs.Lambda.ApiGatewayAuthorizer
     {
         public void Log(string message)
         {
-            
+
         }
 
         public void LogLine(string message)
         {
-            
+
         }
     }
 }
